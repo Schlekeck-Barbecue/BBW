7bd79a7230772368c8521b1f32bcec45acf9e398
NinoBBW <ninofor@hotmail.de>
2020-11-02 13:46:08 +0100

    changed output from "Hello World" to "Hello Leon <3"

--- Test1/src/main/java/edu/post/test/HelloWorld.java
+++ Test1/src/main/java/edu/post/test/HelloWorld.java
@@ -2,6 +2,6 @@ package edu.post.test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Leon <3");
     }
 }
